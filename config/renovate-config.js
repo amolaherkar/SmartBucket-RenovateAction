7a91d53b43cb773e9d79d9fd3ab958c432b1c633
amolaherkar <40294149+amolaherkar@users.noreply.github.com>
2023-03-10 10:13:06 +0530

    Update renovate-config.js

--- config/renovate-config.js
+++ config/renovate-config.js
@@ -4,6 +4,10 @@ module.exports = {
   gitAuthor: 'Renovate Bot <amolaherkar@gmail.com>',
   onboarding: false,
   platform: 'github',
+  logLevel: 'debug',
+  onboardingConfig: {
+    extends: ['config:base'],
+  },
   includeForks: true,
   dryRun: 'full',
   recreateClosed: 'true',
